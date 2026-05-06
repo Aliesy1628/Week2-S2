83188ebb798a27e4d3a08c7f8507ed196450665d
Aliesy1628 <gulaku1628@gmail.com>
2026-05-06 15:16:51 +0700

    Merubah objek

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Elektronik laptop = new Elektronik("Laptop Asus", "Asus", 8500000, 10, 2);
-        Elektronik hp = new Elektronik("HP Samsung", "Samsung", 3200000, 25, 1);
+        Elektronik laptop = new Elektronik("Laptop Asus", "Asus", 8500000, 10,"Elektronik", 2);
+        Elektronik hp = new Elektronik("HP Samsung", "Samsung", 3200000, 25,"Elektronik", 1);
         Makanan Indomie = new Makanan("Indomie Goreng", 3500, 100, "mie instan", "2026-12-01");
         Makanan roti = new Makanan("Roti tawar", 8000, 50, "roti", "2026-04-30");
 
